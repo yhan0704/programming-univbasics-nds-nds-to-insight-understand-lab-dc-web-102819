c7a2f6292ac8c9caf4f606f01b6d2d750c7eea91
Young Han <han20@umbc.edu>
2019-10-22 13:51:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   movie = directors_database[0][:movies]
   index = 0
   while index < movie.length do
-   puts movie[index][:title]
+   movie[index][:title]
     index +=1
   end
 end
