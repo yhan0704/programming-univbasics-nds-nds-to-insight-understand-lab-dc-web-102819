74c8c34e8411e658a80cf6bb591c779931df521f
Young Han <han20@umbc.edu>
2019-10-22 13:34:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,6 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movie = directors_database[0][:movies][:title]
+  movie = directors_database[0][:movies]
   pp movie
 end
